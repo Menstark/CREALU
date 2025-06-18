ac037e7a4e57a6b09a4f253a5099c9b1cdebfd72
Menstark <129077797+Menstark@users.noreply.github.com>
2025-06-18 22:23:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function ajouterPiece() {
+  alert("Fonction pour ajouter une pièce (simulation)");
+}
+function genererPDF() {
+  alert("PDF généré avec contenu structuré (simulation)");
+}
+function envoyerFormulaire() {
+  alert("Formulaire envoyé à alexandre@crealu.com (simulation)");
+}
